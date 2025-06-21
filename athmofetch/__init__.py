68b2d6606ed86267715ce3663d775df0fb770810
ASauvage <a.sauvagepotel@gmail.com>
2025-06-21 15:01:46 +0200

    [feature] action interpretor

--- athmofetch/__init__.py
+++ athmofetch/__init__.py
@@ -1,16 +1,20 @@
-#
-#   /$$$$$$    /$$     /$$                               /$$$$$$$$          /$$               /$$      
-#  /$$__  $$  | $$    | $$                              | $$_____/         | $$              | $$      
-# | $$  \ $$ /$$$$$$  | $$$$$$$  /$$$$$$/$$$$   /$$$$$$ | $$     /$$$$$$  /$$$$$$    /$$$$$$$| $$$$$$$ 
-# | $$$$$$$$|_  $$_/  | $$__  $$| $$_  $$_  $$ /$$__  $$| $$$$$ /$$__  $$|_  $$_/   /$$_____/| $$__  $$
-# | $$__  $$  | $$    | $$  \ $$| $$ \ $$ \ $$| $$  \ $$| $$__/| $$$$$$$$  | $$    | $$      | $$  \ $$
-# | $$  | $$  | $$ /$$| $$  | $$| $$ | $$ | $$| $$  | $$| $$   | $$_____/  | $$ /$$| $$      | $$  | $$
-# | $$  | $$  |  $$$$/| $$  | $$| $$ | $$ | $$|  $$$$$$/| $$   |  $$$$$$$  |  $$$$/|  $$$$$$$| $$  | $$
-# |__/  |__/   \___/  |__/  |__/|__/ |__/ |__/ \______/ |__/    \_______/   \___/   \_______/|__/  |__/
-#                                                                                       By: @ASauvage                                                                                                     
+r"""
+  /$$$$$$    /$$     /$$                               /$$$$$$$$          /$$               /$$      
+ /$$__  $$  | $$    | $$                              | $$_____/         | $$              | $$      
+| $$  \ $$ /$$$$$$  | $$$$$$$  /$$$$$$/$$$$   /$$$$$$ | $$     /$$$$$$  /$$$$$$    /$$$$$$$| $$$$$$$ 
+| $$$$$$$$|_  $$_/  | $$__  $$| $$_  $$_  $$ /$$__  $$| $$$$$ /$$__  $$|_  $$_/   /$$_____/| $$__  $$
+| $$__  $$  | $$    | $$  \ $$| $$ \ $$ \ $$| $$  \ $$| $$__/| $$$$$$$$  | $$    | $$      | $$  \ $$
+| $$  | $$  | $$ /$$| $$  | $$| $$ | $$ | $$| $$  | $$| $$   | $$_____/  | $$ /$$| $$      | $$  | $$
+| $$  | $$  |  $$$$/| $$  | $$| $$ | $$ | $$|  $$$$$$/| $$   |  $$$$$$$  |  $$$$/|  $$$$$$$| $$  | $$
+|__/  |__/   \___/  |__/  |__/|__/ |__/ |__/ \______/ |__/    \_______/   \___/   \_______/|__/  |__/
+                                                                                      By: @ASauvage                                                                                                     
+"""
 
-from requests import get, JSONDecodeError
+from requests import get
+from re import match
+from os import makedirs
 from .utils import get_packages
+from .interpretor import Command
 
 
 MAJOR_VERSION = 1
@@ -18,36 +22,73 @@ MINOR_VERSION = 0
 __version__ = '.'.join((str(MAJOR_VERSION), str(MINOR_VERSION)))
 
 
-def build(config: str, exclude: list) -> None:
-    """
-    Build an Athmosphere structure folder.
-    
-    :param config: Path to the configuration file.
-    :param exclude: List of packages to exclude from the build.
-    """
-    pkgs = get_packages(config, exclude)
+def build(config: str, exclude: list, overwrite: bool = False, verbose: bool = False) -> None:
+    """Build an Athmosphere structure folder"""
+
+    print(__doc__)
+
+    # create folders structure
+    print(":: creating folders structure...", end="")
+    makedirs('tmp/', exist_ok=True)
+    try:
+        makedirs('output/SD/')
+        makedirs('output/payloads/')
+    except FileExistsError:
+        print('Fail\n:: there is already an output folder, please remove it before running this command.')
+        exit(1)
+    print("Done")
+
+
+    pkgs = get_packages(file_path=config, excluded_pkg=exclude)
+    text_version = ""
 
     for pkg in pkgs:
-        print(pkg['name'])
+        print(f":: fetching {pkg['name']}...", end="")
+
+        pkg_data = get(f"https://api.github.com/repos/{pkg['link']}/releases/latest")
+        if not pkg_data.status_code == 200:
+            print(f":: {pkg['name']} not founds.")
+            continue
+        pkg_data = pkg_data.json()
+
+        text_version += f"{pkg['link']}: [{pkg_data['tag_name']}]"
+        for file in pkg['desiredFiles']:
+            link: str
+            for asset in pkg_data['assets']:
+                if match(file['patern'], asset['name']):
+                    link = asset['browser_download_url']
+                    break
+
+            dfile = get(link)
+            with open(f'tmp/{file['filename']}', 'wb') as f:
+                f.write(dfile.content)
+
+            for action in file['actions']:
+                Command(command=action, verbose=verbose)
+                
+        print("Done")
     
 
 def fetch(config: str, exclude: list) -> None:
-    """
-    Build an Athmosphere structure folder.
-    
-    :param config: Path to the configuration file.
-    :param exclude: List of packages to exclude from the build.
-    """
-    pkgs = get_packages(config, exclude)
+    """Build an Athmosphere structure folder"""
 
-    print('╔══════════════════════════════╗',
-          '║        LATEST RELEASE        ║',
-          '╠═════════════════════╦════════╣', sep="\n")
+    pkgs = get_packages(file_path=config, excluded_pkg=exclude)
+
+    print('╔════════════════════════════════╗',
+          '║         LATEST RELEASE         ║',
+          '╠═══════════════════════╦════════╣', sep="\n")
     for pkg in pkgs:
         try:
             version = get(f"https://api.github.com/repos/{pkg['link']}/releases/latest").json()['tag_name']
-        except JSONDecodeError:
+        except KeyError:
             version = "error"
-        print(f"║> {pkg['name']:19}║ {version:>6} ║")
+        print(f"║> {pkg['name']:21}║ {version:>6} ║")
 
-    print('╚═════════════════════╩════════╝')
+    print('╚═══════════════════════╩════════╝')
+
+
+def listpkgs(config: str) -> None:
+    """List available packages"""
+    pkgs = [pkg['name'] for pkg in get_packages(config, [])]
+    
+    print(', '.join(pkgs))
---
+++ athmofetch/interpretor.py
@@ -0,0 +1,74 @@
+from os import getcwd, remove
+from os.path import isfile, isdir
+from shutil import move, copy, rmtree
+from zipfile import ZipFile
+
+
+VARIABLES = dict(
+    TMP_DIR=f"{getcwd()}/tmp",
+    SD_DIR=f"{getcwd()}/output/SD",
+    PAYLOADS_DIR=f"{getcwd()}/output/payloads",
+)
+
+
+class UnknownAction(Exception):
+    def __init__(self, action: str):
+        super().__init__(f"Unknown action: '{action}'")
+
+
+class Command:
+    """
+    <action_key>:<args>
+
+    EXTRACT:<source>,<target>
+    MOVE:<source>,<target>
+    COPY:<source>,<target>
+    REMOVE:<source>
+    """
+    def __init__(self, command: str, verbose: bool = False):
+        self.verbose = verbose
+
+        action_key, args = command.split(':')
+        args = self._args_formater(args)
+
+        match action_key.upper():
+            case 'EXTRACT':
+                self._extract(*args)
+            case 'MOVE':
+                self._move(*args)
+            case 'COPY':
+                self._copy(*args)
+            case 'REMOVE':
+                self._remove(*args)
+            case _:
+                raise UnknownAction(action_key)
+        
+        if self.verbose:
+            print(f":: {action_key} > {','.join(args)}") 
+
+
+    @staticmethod
+    def _args_formater(args: str) -> list[str]:
+        args_list = [s.format(**VARIABLES).lstrip(' ') for s in args.split(',')]
+
+        return args_list
+    
+    def _extract(self, source: str, target: str) -> None:
+        with ZipFile(source, 'r') as zip_ref:
+            zip_ref.extractall(target)
+
+    def _move(self, source: str, target: str) -> None:
+        move(source, target)
+
+    def _copy(self, source: str, target: str) -> None:
+        copy(source, target)
+
+    def _remove(self, source: str) -> None:
+        if isfile(source):
+            remove(source)
+
+        elif isdir(source):
+            rmtree(source)
+
+        else:
+            raise FileNotFoundError(source)
