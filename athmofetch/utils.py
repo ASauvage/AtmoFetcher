81aea5c41209a0a91049f7b67025ac3c23f17d6c
ASauvage <a.sauvagepotel@gmail.com>
2025-06-21 14:53:26 +0200

    [remove useless function

--- athmofetch/utils.py
+++ athmofetch/utils.py
@@ -3,6 +3,12 @@ from json import loads
 
 def get_packages(file_path: str, excluded_pkg: list) -> list:
     """Load a JSON file and return its content as a dictionary."""
+
+    if "athmosphere" in map(str.lower, excluded_pkg):
+        print(":: Warning: 'Athmosphere' is a reserved package and should not be excluded.")
+    if "hekate" in map(str.lower, excluded_pkg):
+        print(":: Warning: 'hekate' is a reserved package and should not be excluded.")
+
     with open(file_path, 'r', encoding='utf-8') as file:
         packages = loads(file.read())
     
