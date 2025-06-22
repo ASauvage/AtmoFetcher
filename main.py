901ab771aa6bae5278af2e92ff378505ef1362b2
Alexandre Sauvage Potel <a.sauvagepotel@gmail.com>
2025-06-22 19:34:16 +0200

    [cleanup] remove useless params

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ if __name__ == "__main__":
     build_ap = sp.add_parser('build', help="Build an Athmosphere structure folder.")
     build_ap.add_argument('-e', '--exclude', nargs='*', default=[],
                           help="Exclude packages from the build.")
-    build_ap.add_argument('-T', '--no-textfile', action='store_true',
-                          help="Avoid generating a versioning textfile.")
-    build_ap.add_argument('-O', '--overwrite', action='store_true',
-                          help="Overwrite the output folder if it exists.")
     
     fetch_ap = sp.add_parser('fetch', help="Fetch package information.")
     fetch_ap.add_argument('-e', '--exclude', nargs='*', default=[],
@@ -32,7 +28,7 @@ if __name__ == "__main__":
 
     match args.command:
         case 'build':
-            atmofetch.build(args.file, args.exclude, args.overwrite, args.verbose)
+            atmofetch.build(args.file, args.exclude, args.verbose)
         case 'fetch':
             atmofetch.fetch(args.file, args.exclude)
         case 'list':
